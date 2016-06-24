bb515f6c82667a9c9ffe17758660e4e5e43102c5
Prisleey <prisleey.araujo@hotmail.com>
2016-06-24 02:13:52 -0300

    Criei Solicitacao

---
+++ src/java/Model/Solicitacao.java
@@ -0,0 +1,66 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+import java.util.Date;
+
+/**
+ *
+ * @author prisl
+ */
+public class Solicitacao {
+    private int id;
+    private Morador morador;
+    private Date dataAbertura;
+    private Date dataFinalizado;
+    private String status;
+
+    public Solicitacao() {
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Morador getMorador() {
+        return morador;
+    }
+
+    public void setMorador(Morador morador) {
+        this.morador = morador;
+    }
+
+    public Date getDataAbertura() {
+        return dataAbertura;
+    }
+
+    public void setDataAbertura(Date dataAbertura) {
+        this.dataAbertura = dataAbertura;
+    }
+
+    public Date getDataFinalizado() {
+        return dataFinalizado;
+    }
+
+    public void setDataFinalizado(Date dataFinalizado) {
+        this.dataFinalizado = dataFinalizado;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    
+}
