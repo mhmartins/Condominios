10f9d10c509737046ed769413b7397eb8770024e
Prisleey <prisleey.araujo@hotmail.com>
2016-06-24 21:08:58 -0300

    teste

--- src/java/Model/Mensagem.java
+++ src/java/Model/Mensagem.java
@@ -10,11 +10,55 @@ package Model;
  * @author prisl
  */
 public class Mensagem {
-    
+
     private int id;
     private String titulo;
     private String texto;
     private Morador morador;
-    
-    
+    private Solicitacao solicitacao;
+
+    public Mensagem(Solicitacao solicitacao) {
+        solicitacao.getMensagens().add(this);
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getTexto() {
+        return texto;
+    }
+
+    public void setTexto(String texto) {
+        this.texto = texto;
+    }
+
+    public Morador getMorador() {
+        return morador;
+    }
+
+    public void setMorador(Morador morador) {
+        this.morador = morador;
+    }
+
+    public Solicitacao getSolicitacao() {
+        return solicitacao;
+    }
+
+    public void setSolicitacao(Solicitacao solicitacao) {
+        this.solicitacao = solicitacao;
+    }
+
 }
